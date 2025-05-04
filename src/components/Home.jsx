7d067b34e344eacfdb79ad88b4a57e6e70a95d04
NarayanaMoorthyM <727822tuio031@skct.edu.in>
2025-05-04 17:45:34 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,13 +25,13 @@ const Home = () => {
   // const { theme, toggleTheme } = themeContext;
 
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1
-  };
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1
+  // };
 
   return (
     <div>
